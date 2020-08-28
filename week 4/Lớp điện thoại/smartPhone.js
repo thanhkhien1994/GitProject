6eccba609bfcd72f966a7f3d5e17dbc6b3c64a99
thanhkhien1994 <thanhkhien1994@gmail.com>
2020-08-28 11:20:28 +0700

    them file smatPhone

---
+++ week 4/Lớp điện thoại/smartPhone.js
@@ -0,0 +1,14 @@
+function Phone(name) {
+    this.name = name;
+    this.battery = 100;
+    this.inbox = [];
+    this.sent = [];
+
+    this.statusPhone = function () {
+
+    }
+    this.statsPin = function () {
+
+    }
+    this.
+}
