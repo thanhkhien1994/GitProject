a5d1a4260d3f1d26de0b761166d7eafe0ae44559
KHIÊN-CODEGYM <you@example.com>
2020-10-01 13:46:32 +0700

    edit

--- Case-Study/quản-Lý-Sinh-Viên/js/variable.js
+++ Case-Study/quản-Lý-Sinh-Viên/js/variable.js
@@ -1,6 +1,14 @@
-let objStudents = localStorage.getItem("savedData") ? JSON.parse(localStorage.getItem("savedData")) : JSON.stringify("Rỗng");
-localStorage.setItem("savedData", JSON.stringify(objStudents));
-
+let objStudents = localStorage.getItem("savedData") ? JSON.parse(localStorage.getItem("savedData")) : JSON.stringify(students);
+let students = [
+    {
+        isName: "Nguyễn Thanh Khiên" ,
+        isClass: "C0620K1",
+        isAge: 22,
+        isAddress: "Mỹ đình, Hà nội",
+        isGender: "Nam",
+        isMail: "Thanhkhien1994@gmail.com/0335915359"
+    }
+]
 let gender = "Nam";
 let _name = document.getElementById("name");
 let _class = document.getElementById("class");
